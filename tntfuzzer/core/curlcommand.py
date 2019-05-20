0f7d098d94020ef88386c06844929430686662b7
Johannes Postler <johannes.postler@txture.io>
2019-05-20 09:43:03 +0200

    Adapt tests according to argparse behaviour

--- tntfuzzer/core/curlcommand.py
+++ tntfuzzer/core/curlcommand.py
@@ -11,10 +11,10 @@ class CurlCommand:
 
     def get(self):
         if not self.data or len(self.data) < 1:
-            curl_command = "curl -X" + self.method.upper() + self.generate_headers() \
+            curl_command = "curl -X" + self.method.upper() + " " + self.generate_headers() \
                            + " " + self.url
         else:
-            curl_command = "curl -X" + self.method.upper() + \
+            curl_command = "curl -X" + self.method.upper() + " " + \
                            self.generate_headers() + " -d '" + self.data + "' " + self.url
 
         return curl_command
