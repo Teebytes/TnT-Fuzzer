f9c8a4a69b1d92748d6561449219c5a0be229907
Pedro Laguna <plaguna@vmware.com>
2019-09-10 15:10:08 +0300

    New Features: Ignore self-signed cert and change host/basePath (#45)
    * New Features: Ignore self-signed cert and change host/basePath
    
    For my workflow I needed to accept self-signed certs and be able to specify different host/basepath than then one specified on the Swagger file.
    
    * Fixed pycodestyle checks
    
    Hopefully this will make the build pass

--- tntfuzzer/tntfuzzer.py
+++ tntfuzzer/tntfuzzer.py
@@ -2,17 +2,17 @@ import argparse
 import json
 import requests
 import termcolor
+import urllib3
 import sys
-
-from urllib.parse import urlparse
 
 from core.curlcommand import CurlCommand
 from core.httpoperation import HttpOperation
 from core.resultvalidatior import ResultValidator
 from core.replicator import Replicator
 from utils.strutils import StrUtils
+from urllib.parse import urlparse
 
-version = "2.2.0"
+version = "2.3.0"
 
 
 class SchemaException(Exception):
@@ -21,13 +21,21 @@ class SchemaException(Exception):
 
 class TntFuzzer:
 
-    def __init__(self, url, iterations, headers, log_unexpected_errors_only, max_string_length, use_string_pattern):
+    def __init__(self, url, iterations, headers, log_unexpected_errors_only,
+                 max_string_length, use_string_pattern, ignore_tls=False, host=None, basepath=None):
         self.url = url
         self.iterations = iterations
         self.headers = headers
         self.log_unexpected_errors_only = log_unexpected_errors_only
         self.max_string_length = max_string_length
         self.use_string_pattern = use_string_pattern
+        self.ignore_tls = ignore_tls
+        self.host = host
+        self.basepath = basepath
+
+        if self.ignore_tls:
+            # removes warnings for insecure connections
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     def start(self):
         print('Fetching open API from: ' + self.url)
@@ -74,7 +82,9 @@ class TntFuzzer:
                                    "documented_reason": "Specification: no schemes entry, fallback to spec URL scheme",
                                    "body": "Specification: host entry not present in Swagger spec"}, '')
 
-        if 'host' in spec:
+        if self.host:
+            host = self.host
+        elif 'host' in spec:
             host = spec['host']
         else:
             host = specURL.netloc
@@ -94,7 +104,9 @@ class TntFuzzer:
                                    "body": "Specification Error: basePath entry missing from Swagger spec"
                                }, '')
             raise SchemaException
-        host_basepath = host + spec['basePath']
+        basePath = self.basepath if self.basepath else spec['basePath']
+        host_basepath = host + basePath
+
         paths = spec['paths']
         type_definitions = spec['definitions']
         # the specifcation can list multiple schemes (http, https, ws, wss) - all should be tested.
@@ -107,13 +119,14 @@ class TntFuzzer:
                 for op_code in path.keys():
                     operation = HttpOperation(op_code, protocol + '://' + host_basepath, path_key,
                                               replicator=replicator, op_infos=path[op_code], use_fuzzing=True,
-                                              headers=self.headers)
+                                              headers=self.headers, ignore_tls=self.ignore_tls)
 
-                    for x in range(self.iterations):
+                    for _ in range(self.iterations):
                         response = operation.execute()
                         validator = ResultValidator()
                         log = validator.evaluate(response, path[op_code]['responses'], self.log_unexpected_errors_only)
-                        curlcommand = CurlCommand(response.url, operation.op_code, operation.request_body, self.headers)
+                        curlcommand = CurlCommand(response.url, operation.op_code, operation.request_body, self.headers,
+                                                  self.ignore_tls)
 
                         # log to screen for now
                         self.log_operation(operation.op_code, response.url, log, curlcommand)
@@ -132,7 +145,8 @@ class TntFuzzer:
                 StrUtils.print_log_row(op_code, url, status_code, documented_reason, body, curlcommand)
 
     def get_swagger_spec(self, url):
-        return json.loads(requests.get(url=url, headers=self.headers).text)
+        verify_tls = not self.ignore_tls  # ignore_tls defaults to False, but verify=False will disable TLS verification
+        return json.loads(requests.get(url=url, headers=self.headers, verify=verify_tls).text)
 
 
 def error_cant_connect():
@@ -141,15 +155,16 @@ def error_cant_connect():
 
 
 def main():
-    print(termcolor.colored('___________  ___________     ___________', color='red'))
-    print(termcolor.colored('\__    ___/__\__    ___/     \_   _____/_ __________________ ___________ ', color='red'))
-    print(termcolor.colored('  |    | /    \|    |  ______ |    __)|  |  \___   /\___   // __ \_  __ \\', color='red'))
-    print(termcolor.colored('  |    ||   |  \    | /_____/ |     \ |  |  //    /  /    /\  ___/|  | \/', color='red'))
-    print(termcolor.colored('  |____||___|  /____|         \___  / |____//_____ \/_____ \\\\___  >__|   ', color='red'))
-    print(termcolor.colored('    Dynamite ', 'green') + termcolor.colored('\/', 'red') +
-          termcolor.colored(' for your API!', 'green') +
-          termcolor.colored('     \/              \/      \/    \/    ', 'red') +
-          termcolor.colored('   v', 'green') + termcolor.colored(version, 'blue'))
+    print(termcolor.colored(r'___________  ___________     ___________', color='red'))
+    print(termcolor.colored(r'\__    ___/__\__    ___/     \_   _____/_ __________________ ___________ ', color='red'))
+    print(termcolor.colored(r'  |    | /    \|    |  ______ |    __)|  |  \___   /\___   // __ \_  __ \\', color='red'))
+    print(termcolor.colored(r'  |    ||   |  \    | /_____/ |     \ |  |  //    /  /    /\  ___/|  | \/', color='red'))
+    print(termcolor.colored(r'  |____||___|  /____|         \___  / |____//_____ \/_____ \\\\___  >__|   ',
+                            color='red'))
+    print(termcolor.colored(r'    Dynamite ', 'green') + termcolor.colored(r'\/', 'red') +
+          termcolor.colored(r' for your API!', 'green') +
+          termcolor.colored(r'     \/              \/      \/    \/    ', 'red') +
+          termcolor.colored(r'   v', 'green') + termcolor.colored(version, 'blue'))
     print('')
 
     parser = argparse.ArgumentParser()
@@ -177,6 +192,15 @@ def main():
     parser.add_argument('--max-random-string-len', dest='max-random-string-len', type=int, default=200,
                         help='The maximum length of generated strings.')
 
+    parser.add_argument('--ignore-cert-errors', dest='ignore-tls', action='store_true', default=False,
+                        help='Ignore TLS errors, like self-signed certificates.')
+
+    parser.add_argument('--host', type=str,
+                        help='Overrides the API host specified within the swagger file.')
+
+    parser.add_argument('--basepath', type=str,
+                        help='Overrides the API basePath specified within the swagger file.')
+
     args = vars(parser.parse_args())
 
     if args['url'] is None:
@@ -184,7 +208,8 @@ def main():
     else:
         tnt = TntFuzzer(url=args['url'], iterations=args['iterations'], headers=args['headers'],
                         log_unexpected_errors_only=not args['log_all'], use_string_pattern=args['string-patterns'],
-                        max_string_length=args['max-random-string-len'])
+                        max_string_length=args['max-random-string-len'], ignore_tls=args["ignore-tls"],
+                        host=args["host"], basepath=args["basepath"])
         try:
             tnt.start()
         except SchemaException:
